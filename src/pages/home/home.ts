2edc271e41e8aeb4a951e3d3265aec2ac554aafe
MrCranito <moreau.totor@gmail.com>
2018-01-22 16:51:24 +0100

    Adding ServiceRoute, getBar From map Camera Location, Adding Icons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,10 +11,13 @@ declare var google;
 
 export class HomePage {
   options : GeolocationOptions;
-  currentPos : Geoposition;
+  currentPos : any;
   places : Array<any> ; 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsService = new google.maps.DirectionsService;
+
   constructor(public navCtrl: NavController,private geolocation : Geolocation) {
       
   }
@@ -24,9 +27,8 @@ export class HomePage {
     };
     this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
 
-        this.currentPos = pos;     
+        this.currentPos = {lat : pos.coords.latitude, lng : pos.coords.longitude};     
 
-        console.log(pos);
         this.addMap(pos.coords.latitude,pos.coords.longitude);
 
     },(err : PositionError)=>{
@@ -48,8 +50,9 @@ export class HomePage {
         }
     
         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-    
-        this.getRestaurants(latLng).then((results : Array<any>)=>{
+        
+        var position = this.currentPos;
+        this.getRestaurants().then((results : Array<any>)=>{
             this.places = results;
             console.log(this.places);
             for(let i = 0 ;i < results.length ; i++)
@@ -59,15 +62,58 @@ export class HomePage {
         },(status)=>console.log(status));
     
         this.addMarker();
+       
+        this.directionsDisplay.setMap(this.map);
+        google.maps.event.addListener(this.map, 'bounds_changed', ()=> {
+            this.getRestaurants().then((results : Array<any>)=>{
+            this.places = results;
+            console.log(this.places);
+            for(let i = 0 ;i < results.length ; i++)
+            {
+                this.createMarker(results[i]);
+            }
+        },(status)=>console.log(status));
+            console.log("bounds_changed");
+        });
     
   }
   createMarker(place)
   {
-      let marker = new google.maps.Marker({
+    var iconBase = '../assets/img/';
+    var icons = {
+          Bar: {
+              icon: iconBase + 'Beer-icon.png'  
+            },
+          night_club: {
+              icon: iconBase + 'Night_club.png'
+            }
+      };
+      var marker;
+      if(place.types[0] == "night_club"){
+            marker = new google.maps.Marker({
       map: this.map,
       animation: google.maps.Animation.DROP,
-      position: place.geometry.location
-      });   
+      position: place.geometry.location,
+      icon : icons["night_club"].icon  
+      });  
+      } 
+      else{
+           marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: place.geometry.location,
+      icon : icons["Bar"].icon  
+      });
+      }
+      let infoWindow = new google.maps.InfoWindow({
+        content: place.name
+        }); 
+      google.maps.event.addListener(marker, 'click', () => {
+              infoWindow.open(this.map, marker);
+              this.getRouteToBar(marker);
+              console.log(this.directionsService);
+
+        });
   }   
   addMarker(){
     
@@ -83,15 +129,17 @@ export class HomePage {
         });
     
         google.maps.event.addListener(marker, 'click', () => {
-        infoWindow.open(this.map, marker);
+              infoWindow.open(this.map, marker);
+              console.log(this.currentPos.coords);
+              console.log(this.directionsService, this.directionsDisplay)
+
         });
-    
   }
-  getRestaurants(latLng)
+  getRestaurants()
   {
       var service = new google.maps.places.PlacesService(this.map);
       let request = {
-          location : latLng,
+          location : this.map.center,
           radius : 500 ,
           types: ['bar','night_club'],
           opennow : true,
@@ -101,6 +149,7 @@ export class HomePage {
               if(status === google.maps.places.PlacesServiceStatus.OK)
               {
                   resolve(results);    
+                  console.log(results);
               }else
               {
                   reject(status);
@@ -109,5 +158,20 @@ export class HomePage {
           }); 
       });
   
+  }
+  getRouteToBar(marker){
+      var Display = this.directionsDisplay;
+      var request = {
+            origin : this.currentPos,
+            destination : marker.position,
+            travelMode : 'WALKING'
+      };
+
+      this.directionsService.route(request, function(result, status){
+      console.log(result);
+          if(status == "OK"){
+                Display.setDirections(result);
+          }
+      })
   }
 }
